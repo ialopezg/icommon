702d6ef4d0c41cce4e93f9056bafad9793a38694
Isidro Lopez <me@ialopezg.com>
2023-09-10 13:36:30 -0400

    fix: import directives and file structures

--- src/common/test/decorators/decorators/greater-than.decorador.spec.ts
+++ src/common/test/decorators/decorators/greater-than.decorador.spec.ts
@@ -1,71 +1,29 @@
+import { GreaterThan } from '../../../decorators';
 import { validate } from 'class-validator';
 import { expect } from 'chai';
 
-import { LessThan } from '../../decorators';
-
-describe('LessThan', () => {
+describe('GreaterThan', () => {
   describe('positive tests', () => {
-    it('should return no ValidationErrors when less or equal than 1', async () => {
+    it('should return no ValidationErrors when greater or equal than 1', async () => {
       class Test {
-        @LessThan(1)
-        value: number;
+        @GreaterThan(1)
+        max: number;
       }
 
       const test = new Test();
-      test.value = 0; // Max is bigger than min => All good!
+      test.max = 10; // Max is bigger than min => All good!
 
       const validationErrorsArray = await validate(test);
-      expect(test.value).greaterThanOrEqual(0); // Sanity check
+      expect(test.max).greaterThanOrEqual(1); // Sanity check
       expect(validationErrorsArray).to.eql([]);
       expect(validationErrorsArray).length(0);
     });
 
-    it('should contain error message when value is greater than threshold', async () => {
+    it('should contain error message when value is less than threshold', async () => {
       const errorMessage = 'Value should be bigger than 0';
 
       class Test {
-        @LessThan(1, {
-          message: errorMessage,
-        })
-        value: number;
-      }
-
-      const test = new Test();
-      test.value = 2;
-
-      const validationErrorsArray = await validate(test);
-      const validationError = validationErrorsArray[0];
-
-      expect(test.value).lessThanOrEqual(2); // Sanity check
-      expect(validationErrorsArray).length(1);
-      expect(validationError.constraints).haveOwnProperty(
-        'lessThan',
-        errorMessage,
-      );
-    });
-  });
-
-  describe('negative tests', () => {
-    it('should return no ValidationErrors when less or equal than -1', async () => {
-      class Test {
-        @LessThan(-1)
-        value: number;
-      }
-
-      const test = new Test();
-      test.value = -2; // Max is bigger than min => All good!
-
-      const validationErrorsArray = await validate(test);
-      expect(test.value).greaterThanOrEqual(-2); // Sanity check
-      expect(validationErrorsArray).to.eql([]);
-      expect(validationErrorsArray).length(0);
-    });
-
-    it('should contain error message when value is greater than threshold', async () => {
-      const errorMessage = 'Value should be bigger than -1';
-
-      class Test {
-        @LessThan(-1, {
+        @GreaterThan(1, {
           message: errorMessage,
         })
         value: number;
@@ -77,10 +35,51 @@ describe('LessThan', () => {
       const validationErrorsArray = await validate(test);
       const validationError = validationErrorsArray[0];
 
-      expect(test.value).lessThanOrEqual(0); // Sanity check
+      expect(test.value).lessThanOrEqual(1); // Sanity check
       expect(validationErrorsArray).length(1);
       expect(validationError.constraints).haveOwnProperty(
-        'lessThan',
+        'greaterThan',
+        errorMessage,
+      );
+    });
+  });
+
+  describe('negative tests', () => {
+    it('should return no ValidationErrors when greater or equal than -1', async () => {
+      class Test {
+        @GreaterThan(-1)
+        value: number;
+      }
+
+      const test = new Test();
+      test.value = 10; // Max is bigger than min => All good!
+
+      const validationErrorsArray = await validate(test);
+      expect(test.value).greaterThanOrEqual(-1); // Sanity check
+      expect(validationErrorsArray).to.eql([]);
+      expect(validationErrorsArray).length(0);
+    });
+
+    it('should contain error message when value is less than threshold', async () => {
+      const errorMessage = 'Value should be bigger than -1';
+
+      class Test {
+        @GreaterThan(-1, {
+          message: errorMessage,
+        })
+        value: number;
+      }
+
+      const test = new Test();
+      test.value = -2;
+
+      const validationErrorsArray = await validate(test);
+      const validationError = validationErrorsArray[0];
+
+      expect(test.value).lessThanOrEqual(-2); // Sanity check
+      expect(validationErrorsArray).length(1);
+      expect(validationError.constraints).haveOwnProperty(
+        'greaterThan',
         errorMessage,
       );
     });
